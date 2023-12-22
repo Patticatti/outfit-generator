d7c37bc464d9b9364d632a834dc63bdb70ae59e8
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-22 02:15:12 -0800

    timeout function

--- global.js
+++ global.js
@@ -1,25 +1,5 @@
-process.stdout.write("Hello  ");
-const questions = ["What is your name", "Whaddya wanna do"];
-const answers = [];
+const waitTime = 3000;
+console.log(`setting a ${waitTime / 1000} second delay`);
+const timerFinished = () => console.log("done");
 
-function ask(i = 0) {
-  process.stdout.write(`\n\n\n ${questions[i]} > `);
-}
-
-ask();
-
-process.stdin.on("data", function (data) {
-  answers.push(data.toString().trim());
-  if (answers.length < questions.length) {
-    ask(answers.length);
-  } else {
-    process.exit();
-  }
-});
-
-process.on("exit", function () {
-  process.stdout.write("\n\n\n\n");
-  process.stdout.write(
-    `Go ${answers[1]} ${answers[0]} you can finish writing answers later`
-  );
-});
+setTimeout(timerFinished, waitTime);
