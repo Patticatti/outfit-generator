9ecc4ae5e7eacdd37c8a01633c20a4bbfb400379
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-21 23:58:40 -0800

    js change

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
-const fs = require('fs');
-const cacheFilePath = 'cache.json';
-const imageInfoArray = [];
-
 document.addEventListener("DOMContentLoaded", function () {
-function handleFileSelect() {
+  // Read data from a JSON file
+  fetch('cache.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Read data from cache.json:', data);
+    })
+    .catch(error => console.error('Error reading file:', error));
+
+  // Write data to a JSON file
+  const jsonData = { example: 'data' };
+  const jsonContent = JSON.stringify(jsonData, null, 2);
+
+  // Use the Fetch API to send data to a server (you may need to set up a server for this)
+  fetch('http://localhost:5500/saveData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonContent,
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('Data successfully saved:', result);
+  })
+  .catch(error => console.error('Error saving data:', error));
+  const imageInfoArray = [];
+
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch('cache.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Read data from cache.json:', data);
+    })
+    .catch(error => console.error('Error reading file:', error));
+
+  // Write data to a JSON file
+  const jsonData = { example: 'data' };
+  const jsonContent = JSON.stringify(jsonData, null, 2);
+
+  // Use the Fetch API to send data to a server (you may need to set up a server for this)
+  fetch('http://localhost:3000/saveData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonContent,
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('Data successfully saved:', result);
+  })
+  .catch(error => console.error('Error saving data:', error));
+  function handleFileSelect() {
     const fileInput = document.getElementById('fileInput');
     const selectedFiles = fileInput.files;
 
@@ -28,6 +76,7 @@ function handleFileSelect() {
         name: imageFile.name,
         dataURL: '', // Placeholder for the data URL (set below)
       };
+      imageInfoArray.push(imageInfo);
     });
 
     // Reset the file input to allow selecting the same files again
@@ -45,29 +94,24 @@ function handleFileSelect() {
     reader.readAsDataURL(imageFile);
     imgElement.alt = imageFile.name;
     imgElement.style.maxWidth = '100px';
-    imageContainer.appendChild(imgElement); //add image to container
+    imageContainer.appendChild(imgElement); // Add image to container
+  }
+
+  function saveImageInfoToJson() {
+    const jsonContent = JSON.stringify(imageInfoArray, null, 2);
+
+    fs.writeFile(cacheFilePath, jsonContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+        return;
+      }
+
+      console.log('JSON file has been updated successfully.');
+    });
+  }
+
+  function finishUpload() {
+    saveImageInfoToJson();
   }
 });
-
-
-function saveImageInfoToJson(imageInfoArray) {
-  const jsonContent = JSON.stringify(imageInfoArray, null, 2);
-  console.log(jsonContent);
-  fs.readFile(cacheFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-  });
-  const jsonData = JSON.parse(data);
-  fs.writeFile(cacheFilePath, updatedJsonString, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return;
-    }
-    const jsonData = JSON.parse(data);
-    jsonData.shirt = 'pink';
-    const updatedJsonString = JSON.stringify(jsonData, null, 2);
-    console.log('JSON file has been updated successfully.');
-  });
-};
+});
