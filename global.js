6c4d1184bb85db3922fa1825e96b8286839c739e
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-22 02:12:29 -0800

    stdin stdout push write on event

--- global.js
+++ global.js
@@ -1,13 +1,25 @@
 process.stdout.write("Hello  ");
-const questions = ["What is your name", "You stink"];
+const questions = ["What is your name", "Whaddya wanna do"];
 const answers = [];
 
-function ask(i) {
+function ask(i = 0) {
   process.stdout.write(`\n\n\n ${questions[i]} > `);
 }
 
+ask();
+
 process.stdin.on("data", function (data) {
-  process.stdout.write(data.toString().trim());
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
 });
 
-ask(answers.length);
+process.on("exit", function () {
+  process.stdout.write("\n\n\n\n");
+  process.stdout.write(
+    `Go ${answers[1]} ${answers[0]} you can finish writing answers later`
+  );
+});
