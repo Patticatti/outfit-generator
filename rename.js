aa285b10580c8f71a1288fc923e8001ccd21b5ee
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-27 14:27:31 -0800

    remove file with unlinksync

--- rename.js
+++ rename.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 
 //fs.renameSync("./lib/config.js", "./lib/project-config.js");
 
-console.log("renamed file");
+//console.log("renamed file");
 
-fs.rename("./lib/notes.md", "./notes.md", function (err) {
+// fs.rename("./lib/notes.md", "./notes.md", function (err) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Notes markdown file moved");
+// });
+
+//fs.unlinkSync("./lib/project-config.js");
+fs.unlink("notes.md", function (err) {
   if (err) {
     throw err;
   }
-  console.log("Notes markdown file moved");
+  console.log("removed notes.md");
 });
