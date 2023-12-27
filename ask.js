afaa6edcc5fe505606ebacb777bb1561636d17c4
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-27 15:52:38 -0800

    ask fix

--- ask.js
+++ ask.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+
+let answerStream;
+
 let questions = [
   "What is your name?",
   "Where do you live?",
@@ -11,15 +15,20 @@ function ask(i) {
   process.stdout.write(` > `);
 }
 
+process.on("exit", function () {
+  process.stdout.write("\n\n\n\n ");
+  process.stdout.write(
+    `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]}`
+  );
+  process.stdout.write("\n\n\n\n");
+});
+
 process.stdin.on("data", function (data) {
   answers.push(data.toString().trim());
+
   if (answers.length < questions.length) {
     ask(answers.length);
   } else {
     process.exit();
   }
-});
-
-process.on("exit", function () {
-  process.stdout.write("\n\n\n\n ");
 });
