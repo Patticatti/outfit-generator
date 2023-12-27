807db3f037a482cdace39fbbe4653741be05e376
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-27 15:14:42 -0800

    move all files from dir, delete dir

--- directories.js
+++ directories.js
@@ -3,10 +3,17 @@ const fs = require("fs");
 //fs.renameSync("./assets/logs.js", "boob/logs.js");
 //console.log("logs folder moved");
 
-fs.rmdir("./assets", function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("./assets directory removed");
-  }
+// fs.rmdir("./assets", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("./assets directory removed");
+//   }
+// });
+
+fs.readdirSync("./boob").forEach((file) => {
+  fs.renameSync(`./boob/${file}`, `./lib/${file}`);
 });
+console.log("Files Removed");
+fs.rmdirSync("./boob");
+console.log("Folder removed");
