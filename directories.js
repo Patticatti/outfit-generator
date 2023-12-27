2309595ce51ac71bf31fe316a3fde3f7d45b4325
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-27 14:47:22 -0800

    removed directory and renamesync

--- directories.js
+++ directories.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 
-fs.renameSync("./assets/logs.js", "boob/logs.js");
+//fs.renameSync("./assets/logs.js", "boob/logs.js");
+//console.log("logs folder moved");
 
-console.log("logs folder moved");
+fs.rmdir("./assets", function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("./assets directory removed");
+  }
+});
