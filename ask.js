b1a3ebf53e6e0a8c5cb8267786ed92f3e02295b2
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-27 16:01:32 -0800

    use stream to create file and log answers

--- ask.js
+++ ask.js
@@ -15,20 +15,37 @@ function ask(i) {
   process.stdout.write(` > `);
 }
 
-process.stdin.on("data", function (data) {
-  answers.push(data.toString().trim());
-
-  if (answers.length < questions.length) {
-    ask(answers.length);
-  } else {
-    process.exit();
+process.stdin.once("data", (data) => {
+  let name = data.toString().trim();
+  let fileName = `./${name}.md`;
+  if (fs.existsSync(fileName)) {
+    fs.unlinkSync(fileName);
   }
+  answerStream = fs.createWriteStream(fileName);
+  answerStream.write(`Question Answers for ${name}\n======\n`);
+});
+
+process.stdin.on("data", function (data) {
+  let answer = data.toString().trim();
+  answerStream.write(`Question: ${questions[answers.length]}\n`);
+
+  answerStream.write(`Answer: ${answer}\n`, function () {
+    if (answers.length < questions.length) {
+      ask(answers.length);
+    } else {
+      process.exit();
+    }
+  });
+  answers.push(answer);
 });
 
 process.on("exit", function () {
+  answerStream.close();
   process.stdout.write("\n\n\n\n ");
   process.stdout.write(
     `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]}`
   );
   process.stdout.write("\n\n\n\n");
 });
+
+ask(answers.length);
---
+++ patti.md
@@ -0,0 +1,8 @@
+Question Answers for patti
+======
+Question: What is your name?
+Answer: patti
+Question: Where do you live?
+Answer: van
+Question: What are you going to do with nose.js?
+Answer: idk
